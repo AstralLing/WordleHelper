dd60139351d4648a114342e71b2ba21ac07310cc
AstralLing <84491337+AstralLing@users.noreply.github.com>
2022-03-08 10:25:52 +0800

    alpha2.0.1.220308
    cli upd

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include "wlh.h"
+
+unsigned int simpleHash(char *s) {
+  unsigned int result = 0;
+  for (int i = 0; i < strlen(s); ++i)
+    result = (result << 11) ^ (result >> 4) ^ s[i];
+  return result ^ (result >> 5) ^ (result >> 14);
+}
+std::mt19937 randGenerator(simpleHash("NeverKnowsBest."));
+int randNum(int l, int r) { return std::uniform_int_distribution<int>(l, r)(randGenerator); }
+
+int main(int argc, char *argv[]) {
+  char optionName;
+  int option = 0;
+
+  while (--argc > 0 && (*++argv)[0] == '-') {
+    while (optionName = *++argv[0]) {
+      switch (optionName) {
+        case 'W': // Wordle
+          option |= 1 << 0;
+          break;
+        case 'N': // Numberle
+          option |= 1 << 1;
+          break;
+        case 'n': // New game
+          option |= 1 << 2;
+          break;
+        case 'c': // Continue game
+          option |= 1 << 3;
+          break;
+        case 'd':
+          option |= 1 << 10;
+          break;
+        case 'h':
+          option |= 1 << 30;
+          break;
+        default:
+          printf("Found: illegal optional %c\n", optionName);
+          argc = 0;
+          option |= 1 << 31;
+          break;
+      }
+    }
+  }
+
+  if (option >> 31) {
+    std::cout << "Illegal option found, try again or use -help for help.\n";
+  } else if (!option || option & (1 << 30)) {
+    std::cout
+        << "    -W: play a wordle game. (default)\n"
+           "    -N: play a numberle game.\n"
+           "    -n: start a new game, the program will solve it automatically. (default)\n"
+           "    -c: continue your game, input the information you already got and "
+           "the program will show advice.\n"
+           "    -d: show details during game.\n";
+  } else if (option & (1 << 0) && option & (1 << 1) ||
+             option & (1 << 2) && option & (1 << 3)) {
+    std::cout << "Option conflicted, use -help for help.\n";
+  } else {
+    // Set default
+    if (!((option & (1 << 2)) | (option & (1 << 3))))
+      option |= 1 << 2;
+
+    if (!((option & (1 << 0)) | (option & (1 << 1))))
+      option |= 1 << 0;
+
+    if (option & (1 << 0)) {
+      playWordle(option);
+    } else {
+      std::cout << "Not done yet.\n";
+      // playNumberle(option);
+    }
+  }
+  return 0;
+}
---
+++ wlh.h
@@ -0,0 +1,16 @@
+#ifndef __WLH_H__
+#define __WLH_H__
+
+#include <array>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <vector>
+void playNumberle(int);
+void playWordle(int);
+unsigned int simpleHash(char *);
+int randNum(int, int);
+
+#endif
